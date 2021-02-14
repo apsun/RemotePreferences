7030514a885824bda5b558cc6d9b6472d527a377
Andrew Sun <andrew@crossbowffs.com>
2021-02-14 13:44:14 -0800

    Switch build.gradle to Kotlin

---
+++ library/build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    id("com.android.library")
+    id("maven-publish")
+    id("signing")
+}
+
+android {
+    compileSdkVersion(30)
+
+    defaultConfig {
+        minSdkVersion(1)
+        targetSdkVersion(30)
+        versionCode(8)
+        versionName("0.8")
+    }
+}
+
+val sourcesJar = tasks.register("sourcesJar", Jar::class) {
+    from(android.sourceSets["main"].java.getSourceFiles())
+    archiveClassifier.set("sources")
+}
+
+val javadoc = tasks.register("javadoc", Javadoc::class) {
+    source = android.sourceSets["main"].java.getSourceFiles()
+    classpath += project.files(android.bootClasspath)
+}
+
+val javadocJar = tasks.register("javadocJar", Jar::class) {
+    dependsOn(javadoc)
+    from(javadoc)
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    publications {
+        afterEvaluate {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                artifact(sourcesJar)
+                artifact(javadocJar)
+
+                groupId = "com.crossbowffs.remotepreferences"
+                artifactId = "remotepreferences"
+                version = android.defaultConfig.versionName
+
+                pom {
+                    packaging = "aar"
+                    name.set("RemotePreferences")
+                    description.set("A drop-in solution for inter-app access to SharedPreferences on Android.")
+                    url.set("https://github.com/apsun/RemotePreferences")
+                    licenses {
+                        license {
+                            name.set("MIT")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set("Andrew Sun")
+                            email.set("andrew@crossbowffs.com")
+                        }
+                    }
+                    scm {
+                        url.set(pom.url.get())
+                        connection.set("scm:git:${url.get()}.git")
+                        developerConnection.set("scm:git:${url.get()}.git")
+                    }
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                username = project.findProperty("ossrhUsername") as String?
+                password = project.findProperty("ossrhPassword") as String?
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    afterEvaluate {
+        sign(publishing.publications["release"])
+    }
+}
